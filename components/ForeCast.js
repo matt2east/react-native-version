fa897c9f39577c4648f19c29eaf46ae5bd1f3ec0
Alona Varshal <thirdwave72@gmail.com>
2018-11-14 00:21:30 -0600

    Add weather api

--- components/ForeCast.js
+++ components/ForeCast.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Platform, View, Text, ScrollView, StyleSheet } from "react-native";
 import { Constants, Location, Permissions } from "expo";
 import axios from "axios";
-import AIR_NOW_API from "../utils/secret.js";
+import { AIR_NOW_API, WEATHER_API } from "../utils/secret.js";
 import { AirQuality } from "./AirQuality.js";
 
 class ForeCast extends React.Component {
   state = {
-    location: null,
     errorMessage: null,
-    address: null,
-    data: []
+    location: null,
+    postalCode: null,
+    aqi: [],
+    weather: {}
   };
 
   _getLocationsAndDataAsync = async () => {
@@ -22,25 +23,44 @@ class ForeCast extends React.Component {
     }
 
     let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
 
     const { latitude, longitude } = await location.coords;
-    let [address] = await Location.reverseGeocodeAsync({
+    const [address] = await Location.reverseGeocodeAsync({
       latitude,
       longitude
     });
     const { postalCode } = address;
 
+    this.setState({
+      location,
+      postalCode
+    });
+
+    // Get AirNow data
     let currentDate = new Date();
     currentDate = currentDate.toISOString().split("T")[0];
     const encodedURI = window.encodeURI(
       `http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${postalCode}&date=${currentDate}&distance=25&API_KEY=${AIR_NOW_API}`
     );
-    const { data } = await axios.get(encodedURI);
-    this.setState({
-      address,
-      data
-    });
+
+    // Get Weather
+    const encodedURIWeather = window.encodeURI(
+      `https://api.darksky.net/forecast/${WEATHER_API}/${latitude},${longitude}`
+    );
+
+    axios
+      .all([axios.get(encodedURI), axios.get(encodedURIWeather)])
+      .then(
+        axios.spread((aqiRes, weatherRes) => {
+          const aqi = aqiRes.data;
+          const weather = weatherRes.data;
+          this.setState({
+            aqi,
+            weather
+          });
+        })
+      )
+      .catch(err => console.log(err));
   };
 
   componentDidMount() {
@@ -61,19 +81,25 @@ class ForeCast extends React.Component {
     } else if (this.state.address) {
       text = "Today's Conditions";
     }
+    console.log(this.state);
     return (
       <View style={styles.container}>
-        <View>
+        {/* <View>
           <Text style={styles.headerTab}>{text}</Text>
           {!this.state.data[0] ? (
             <Text style={styles.header}>AIR QUALITY</Text>
           ) : (
-            <AirQuality
-              aqi={this.state.data[0].AQI}
-              name={this.state.data[0].Category.Name.toUpperCase()}
-            />
+            <View>
+              <AirQuality
+                aqi={this.state.data[0].AQI}
+                name={this.state.data[0].Category.Name.toUpperCase()}
+              />
+              <View>{JSON.stringify(this.state.weather, null, 2)}</View>
+            </View>
           )}
-        </View>
+        </View> */}
+        <View>{JSON.stringify(this.state.location, null, 2)}</View>
+        {/* <View>{JSON.stringify(this.state.weather, null, 2)}</View> */}
       </View>
     );
   }
