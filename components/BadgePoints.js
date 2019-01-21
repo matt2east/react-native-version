c731d2e0281f2c6669b237ea756d73d885a1c0cd
matthew <matthew@MACs-MacBook-Pro.local>
2019-01-20 21:23:04 -0500

    badge count saved into local storage

--- components/BadgePoints.js
+++ components/BadgePoints.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, Alert, Button, StyleSheet, View } from "react-native";
+import { Text, Alert, Button, StyleSheet, View,  AsyncStorage } from "react-native";
 
 class BadgePoints extends Component {
   constructor(props) {
@@ -10,6 +10,17 @@ class BadgePoints extends Component {
     console.log("incrementCount");
     this.setState({ count: this.state.count + 1 });
     console.log(this.state.count);
+    let badgeValue = this.state.count;
+    let badge_object = {
+        badgeKey: badgeValue
+      };
+    AsyncStorage.setItem("badgeCount", JSON.stringify(badge_object), () => {
+        AsyncStorage.getItem("badgeCount", (err, result) => {
+          console.log("badgeCount result is " +result);
+          if (err) console.log(err);
+        });
+      });
+
   };
   render() {
     return <Button onPress={this.incrementCount} title="Do Challenge!" />;
