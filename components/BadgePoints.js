041c2db0c7d3237d2eb4c863f4613e60612a10c3
matthew <matthew@MACs-MacBook-Pro.local>
2019-01-20 21:15:12 -0500

    added state count increment logic

--- components/BadgePoints.js
+++ components/BadgePoints.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import { Text, Alert, Button, StyleSheet, View } from "react-native";
 
 class BadgePoints extends Component {
-  handleSubmit = () => {
-    console.log("you pressed the badge button");
+  constructor(props) {
+    super(props);
+    this.state = { count: 0, isHidden: false };
+  }
+  incrementCount = () => {
+    console.log("incrementCount");
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
   };
-
   render() {
-    return <Button onPress={this.handleSubmit} title="Do Challenge!" />;
+    return <Button onPress={this.incrementCount} title="Do Challenge!" />;
   }
 }
 
